7a13487735ba32b5a5f1197bf807c49d3986aea7
MohanPrasad1111 <50876683+MohanPrasad1111@users.noreply.github.com>
2021-11-04 11:08:57 +0530

    Create SpringIntegrationRunner.java

---
+++ SpringIntegrationRunner.java
@@ -0,0 +1,9 @@
+package com.tuc.br.assemblytest.test.suite;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.tuc.br.assemblytest.BrTuOnlineAssemblygatewayApplication;
+
+@SpringBootTest(classes = BrTuOnlineAssemblygatewayApplication.class)
+public class SpringIntegrationRunner {
+}
